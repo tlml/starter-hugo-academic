9fe0369cb1f2662a686a3b48851f137984b89d66
tlml <tlml@users.noreply.github.com>
2022-11-23 17:20:06 +0000

    Added solarpub list page regeneration instructions

--- update_site.sh
+++ update_site.sh
@@ -1,7 +1,11 @@
 echo Building star website...
 hugo --baseURL "http://www.star.uclan.ac.uk/~tl" --destination ~/public_html
 chmod go+r -R  ~/public_html
+mkdir ~/public_html/solarpubs/
 find ~/public_html -type d | xargs -i chmod go+x {}
+echo .
+echo Remember to run /net/tauri2/work/tl/doc/JHI_pubs/solarpubscript/solar_bib2html.pl
+echo .
 echo Building for tlml.github.io
 hugo
 echo "To push to tlml.github.io do:"
